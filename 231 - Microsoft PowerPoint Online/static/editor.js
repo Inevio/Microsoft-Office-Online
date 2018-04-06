381662113d9cb7482703a24663d69ef72967e9d9
Carlos Navas <carlosnavas1991@gmail.com>
2018-04-06 11:24:29 +0200

    PowerPoint working and css fixes

--- 231 - Microsoft PowerPoint Online/static/editor.js
+++ 231 - Microsoft PowerPoint Online/static/editor.js
@@ -4,40 +4,54 @@ var dontWait     = false;
 var win          = $(this);
 var width        = api.tool.desktopWidth();
 var height       = api.tool.desktopHeight() - 70;
-var windowObject = api.popup( 'https://static.inevio.com/app/231/editor.html', width, height ).render();
 
-var timer = setInterval( function(){
+var isElectron   = false
 
-  if( windowObject.closed ){
+var userAgent = navigator.userAgent.toLowerCase()
+if (userAgent.indexOf(' electron/') > -1) {
+   // Electron-specific code
+   isElectron = true
+   win.addClass('isElectron')
+}
 
-    api.view.remove();
-    clearInterval( timer );
+if(!isElectron){
 
+  var windowObject = api.popup( 'https://static.inevio.com/app/231/editor.html', width, height ).render();
+
+  var timer = setInterval( function(){
+
+    if( windowObject.closed ){
+
+      api.view.remove();
+      clearInterval( timer );
+
+    }
+
+  }, 500 );
+
+  var _close = function(){
+    api.app.removeView( win );
+  };
+
+  // Events
+  win
+  .on( 'ui-view-focus', function(){
+    windowObject.focus();
+  })
+
+  .on( 'ui-view-removed', function(){
+    windowObject.close();
+  });
+
+  $( (function(){return this;})() ).on( 'beforeunload', function(){
+    windowObject.close();
+  });
+
+  // Start
+  if( !params || params.command !== 'openFile' ){
+    return api.app.removeView( win );
   }
 
-}, 500 );
-
-var _close = function(){
-  api.app.removeView( win );
-};
-
-// Events
-win
-.on( 'ui-view-focus', function(){
-  windowObject.focus();
-})
-
-.on( 'ui-view-removed', function(){
-  windowObject.close();
-});
-
-$( (function(){return this;})() ).on( 'beforeunload', function(){
-  windowObject.close();
-});
-
-// Start
-if( !params || params.command !== 'openFile' ){
-  return api.app.removeView( win );
 }
 
 if( typeof params.data !== 'object' ){
@@ -66,8 +80,13 @@ params.data.done( function( id ){
           return alert( lang.openFileError, _close );
         }
 
+        if(isElectron){
+          $('webview').attr('src', 'https://static.inevio.com/app/231/editor.html?id=' + encodeURIComponent( 'gdrive:' + params.gdrive + ':' + id )  + '&empty=0')
+        }else{
+          windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + encodeURIComponent( 'gdrive:' + params.gdrive + ':' + id )  + '&empty=0'
+        }
+
         // To Do -> Improve check like horbito's files
-        windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + encodeURIComponent( 'gdrive:' + params.gdrive + ':' + id )  + '&empty=0';
 
       })
 
@@ -90,8 +109,13 @@ params.data.done( function( id ){
           return alert( lang.openFileError, _close );
         }
 
+        if(isElectron){
+          $('webview').attr('src', 'https://static.inevio.com/app/231/editor.html?id=' + encodeURIComponent( 'dropbox:' + params.dropbox + ':' + id )  + '&empty=0')
+        }else{
+          windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + encodeURIComponent( 'dropbox:' + params.dropbox + ':' + id )  + '&empty=0'
+        }
+
         // To Do -> Improve check like horbito's files
-        windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + encodeURIComponent( 'dropbox:' + params.dropbox + ':' + id )  + '&empty=0';
 
       })
 
@@ -107,8 +131,13 @@ params.data.done( function( id ){
           return alert( lang.openFileError, _close );
         }
 
+        if(isElectron){
+          $('webview').attr('src', 'https://static.inevio.com/app/231/editor.html?id=' + encodeURIComponent( 'onedrive:' + params.onedrive + ':' + id )  + '&empty=0')
+        }else{
+          windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + encodeURIComponent( 'onedrive:' + params.onedrive + ':' + id )  + '&empty=0'
+        }
+
         // To Do -> Improve check like horbito's files
-        windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + encodeURIComponent( 'onedrive:' + params.onedrive + ':' + id )  + '&empty=0';
 
       })
 
@@ -129,9 +158,21 @@ params.data.done( function( id ){
         }
 
         if( !formats.original || !formats.original.size ){
-          windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + id + '&empty=1';
+
+          if(isElectron){
+            $('webview').attr('src', 'https://static.inevio.com/app/231/editor.html?id=' + id + '&empty=1')
+          }else{
+            windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + id + '&empty=1'
+          }
+
         }else if( VALID_MIMES.indexOf( formats.original.mime ) !== -1 ){
-          windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + id + '&empty=0';
+
+          if(isElectron){
+            $('webview').attr('src', 'https://static.inevio.com/app/231/editor.html?id=' + id + '&empty=0')
+          }else{
+            windowObject.location.href = 'https://static.inevio.com/app/231/editor.html?id=' + id + '&empty=0'
+          }
+
         }else{
           alert( lang.openFileError, _close );
         }
